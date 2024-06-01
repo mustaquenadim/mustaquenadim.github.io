49e009fadecf95ad63db05579afc4fb6ee5af559
mustaquenadim <mustaquenadim2000@gmail.com>
2024-06-01 11:10:06 +0600

    [Removed] Resume button

--- src/components/nav.js
+++ src/components/nav.js
@@ -259,7 +259,7 @@ const Nav = ({ isHome }) => {
                 </TransitionGroup>
               </ol>
 
-              <TransitionGroup component={null}>
+              {/* <TransitionGroup component={null}>
                 {isMounted && (
                   <CSSTransition classNames={fadeDownClass} timeout={timeout}>
                     <div style={{ transitionDelay: `${isHome ? navLinks.length * 100 : 0}ms` }}>
@@ -267,7 +267,7 @@ const Nav = ({ isHome }) => {
                     </div>
                   </CSSTransition>
                 )}
-              </TransitionGroup>
+              </TransitionGroup> */}
             </StyledLinks>
 
             <TransitionGroup component={null}>
